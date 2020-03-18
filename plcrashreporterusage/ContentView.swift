8696ce60d80e21039ae91b2b2ce94fbe11417dd8
Marco Eidinger <marco.eidinger@sap.com>
2020-03-18 09:38:50 -0700

    Initial Commit

---
+++ plcrashreporterusage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  plcrashreporterusage
+//
+//  Created by Eidinger, Marco on 3/18/20.
+//  Copyright © 2020 Eidinger, Marco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
